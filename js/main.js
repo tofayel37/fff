dbc68d1d5595b55f0bb7d44519043597cc789f83
Tofayel <107603521+tofayel37@users.noreply.github.com>
2022-06-29 22:22:20 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$('#one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('#two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:false,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
